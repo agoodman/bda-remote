55df2c4fcc29baa7bed3033f3718e1fd5bb1a680
Aubrey Goodman <aubrey.goodman@gmail.com>
2020-07-05 18:08:23 -0700

    added records#batch action

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,21 @@ class RecordsController < ApplicationController
   include Serviceable
   skip_before_action :verify_authenticity_token
   acts_as_service :record
+
+  def batch
+    @records = params['records']
+    if @records.count > 100
+      head :bad_request && return
+    end
+    @records.each do |p|
+      rp = record_params(p)
+      next unless rp
+      e = Record.new(rp)
+      e.save
+    end
+  end
+
+  def record_params(input)
+    input.permit(:player, :competition_id, :hits, :kills, :deaths, :distance, :weapon)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :records
+  resources :records do
+    collection do
+      post :batch
+    end
+  end
+  resources :competitions do
+    resources :records, only: :index
+  end
 end
