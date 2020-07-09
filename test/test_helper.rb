1211233879e2d543f1ba33bdd7e1acdf652c5a16
Aubrey Goodman <aubrey.goodman@gmail.com>
2020-07-08 21:05:27 -0700

    Added shoulda gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,11 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :minitest
+    with.library :rails
+  end
+end
+
