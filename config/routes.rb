b51cbb1cac97aec14e53ff96d8216f57c82ce8e3
Aubrey Goodman <aubrey.goodman@gmail.com>
2020-08-17 18:56:09 -0700

    Disable devise routes to fix prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+#  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
   root to: 'welcome#index'
 
