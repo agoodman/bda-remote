77c8816b08b66dfd45a40801a00946efc5416e7d
Aubrey Goodman <aubrey.goodman@gmail.com>
2020-08-19 18:46:59 -0700

    Cleaned up s3 config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,7 @@
 unless ENV['AWS_CLIENT_ID'].nil? || ENV['AWS_CLIENT_SECRET'].nil?
   Aws.config.update({
-    region: 'us-east-1',
+    region: ENV['AWS_REGION'],
     credentials: Aws::Credentials.new(ENV['AWS_CLIENT_ID'], ENV['AWS_CLIENT_SECRET'])
   }) 
-
-  Bucket = Aws::S3::Resource.new.buckets[ENV['S3_BUCKET']]
-else
-  Bucket = nil
 end
 
