1d415b7cf972ccff9ac1504010df8fd9ad12f99f
Aubrey Goodman <aubrey.goodman@gmail.com>
2020-08-22 13:17:28 -0700

    Show only file part of craft url

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -1,2 +1,7 @@
 module CompetitionsHelper
+
+  def vessel_file_name_for_player(competition, player)
+    competition.vessels.where(player_id: player.id).first.craft_url.split("/").last
+  end
+
 end
