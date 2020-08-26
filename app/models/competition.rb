001f24ddfdeabb671f475c8b9b52b706a69f6a23
Aubrey Goodman <aubrey.goodman@gmail.com>
2020-08-25 17:14:00 -0700

    Use sort_by with lambda

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -134,7 +134,7 @@ class Competition < ApplicationRecord
         hits: e.map(&:hits).sum,
         name: (vessels.where(id: k).first.player.name rescue "-")
       }
-    }.sort(:kills)
+    }.sort_by { |e| e[:kills] }
   end
 
 end
