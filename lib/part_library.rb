cc35293d29d537d58674193f0aa3913162b50ca2
Aubrey Goodman <aubrey.goodman@gmail.com>
2020-12-10 21:48:43 -0800

    removed unnecessary logging

--- lib/part_library.rb
+++ lib/part_library.rb
@@ -7,12 +7,12 @@ module PartLibrary
       eidx = body.lines.find_index { |e| e =~ /MODULE/ }
       part_lines = body.lines[0..eidx]
       part_lines.each do |e|
-        puts "in: #{e}"
+        # puts "in: #{e}"
         next if e =~ /\/\//
         results = /\s*(.+) = (.+)/.match(e)
         next if results.nil?
         if valid_keys.include?(results[1].to_sym)
-          puts "found: '#{results[1]}' = '#{results[2]}'"
+          # puts "found: '#{results[1]}' = '#{results[2]}'"
           props[results[1]] = results[2]
         end
       end
