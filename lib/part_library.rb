50cc06fea6aaef138728222a6f22401adf14afdb
Aubrey Goodman <aubrey.goodman@gmail.com>
2020-12-14 08:23:30 -0800

    strip whitespace from part property values

--- lib/part_library.rb
+++ lib/part_library.rb
@@ -9,11 +9,11 @@ module PartLibrary
       part_lines.each do |e|
         # puts "in: #{e}"
         next if e =~ /\/\//
-        results = /\s*(.+) = (.+)/.match(e)
+        results = /\s*(.+) = (.+)$/.match(e)
         next if results.nil?
         if valid_keys.include?(results[1].to_sym)
           # puts "found: '#{results[1]}' = '#{results[2]}'"
-          props[results[1]] = results[2]
+          props[results[1]] = results[2].strip
         end
       end
       return props
